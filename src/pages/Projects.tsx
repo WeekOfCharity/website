cdafd627970272ee101179814e9526cac1e37e5a
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2023-07-29 00:23:12 +0200

    Add first layout version of the Projects page
    Co-authored-by: BinaKleinerAls3 <BinaKleinerAls3@users.noreply.github.com>

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,12 @@
 import classNames from 'classnames';
 import { Brush4 } from '../components/Brushes/Brush4';
 import { getDocumentTitle } from '../utils/getDocumentTitle';
+import './Projects.scss';
 
-const arrowDown = new URL('../assets/arrow-down.svg', import.meta.url);
+const chesster1 = new URL('../assets/chesster-1.png', import.meta.url);
+const chesster2 = new URL('../assets/chesster-2.png', import.meta.url);
+const chesster3 = new URL('../assets/chesster-3.png', import.meta.url);
+const chesster4 = new URL('../assets/chesster-4.png', import.meta.url);
 
 export const Projects = () => {
 
@@ -11,7 +15,7 @@ export const Projects = () => {
   return (
     <main className="text-neutral-800 woc-accent-persian">
       <header className="px-5 py-20 relative text-center">
-        <div className="font-round2 font-bold text-accent-900 uppercase">Von euch und uns</div>
+        <div className="font-round2 font-bold text-accent-900 uppercase">Über uns</div>
 
         <div className="font-pally font-bold max-w-screen-md mx-auto my-5 text-accent-500 text-4xl md:text-7xl w-4/5">
           Die Week of Charity
@@ -20,27 +24,64 @@ export const Projects = () => {
         <Brush4 className="absolute h-96 left-1/2 mt-8 text-neutral-100 top-1/2 transform-gpu -translate-x-1/2 -translate-y-1/2 w-auto -z-10" />
       </header>
 
-      <div className="max-w-screen-lg my-20 mx-auto space-y-20 md:space-y-40">
-          <section className="bg-persian-100 bg-opacity-50 pb-5 pt-10 row-span-2">
-            <div className="font-round2 font-bold -rotate-[10deg] -skew-x-[10deg] text-persian-900 text-center transform-gpu uppercase">Week of Was?</div>
-            {/* <div className="flex font-semibold justify-center max-w-screen-md mx-auto py-10 relative text-3xl md:text-5xl w-4/5">
-              <span className="z-10">Über das Projekt</span>
-              <Brush5 className="absolute h-12 left-1/2 -rotate-2 text-persian-900 text-opacity-75 top-8 md:top-10 -translate-x-1/2 w-auto" />
-            </div> */}
-            <div className="font-semibold max-w-screen-md mx-auto pt-10 text-3xl md:text-4xl text-center w-4/5">Über das Projekt</div>
-
-            <div className="leading-relaxed mx-5 text-center">
-              Willkommen! Wir sind die Week of Charity, ein Dauerstreamprojekt für einen guten Zweck! Eine Woche lang wird abwechselnd auf den Twitch-Kanälen unserer Mitglieder
-              durchgängig gestreamt, um Spenden zu sammeln. Das Programm ist breit gefächert und neben diversen Videospielen wird unter anderem gezeichnet, Dungeons and Dragons
-              gespielt und es werden Quizshows abgehalten.
-              <br />
-              <br />
-              Alle Spenden, die in dieser Woche gesammelt werden, gehen dieses Jahr an das Tierheim Berlin. Unser Projekt wurde zuerst 2021 auf die Beine gestellt und geht in
-              diesem Jahr schon in die dritte Runde! Im Programm sind fast alle Streamer*Innen des letzten Jahres + einige Neuzugänge dabei. Wir hoffen, die Woche wird euch genau
-              so viel Freude und Unterhaltung bringen wie uns. Wir freuen uns auf euch! Mehr zu unserem Projekt findet ihr <a href="/projekte" className="cursor-pointer text-persian-500">hier</a>.
+      <div className="max-w-screen-2xl mt-5 mb-40 mx-auto flex place-content-center">
+        <div className="max-w-screen-lg mx-10 md:mx-32 lg:mx-56">
+          <section className="flex bg-persian-100 bg-opacity-50 py-10 row-span-2">
+            <div>
+              <div className="leading-relaxed mx-5 md:mx-10 text-left">
+                <div className="inline chesster-1-wrapper">
+                  <img className="chesster-1" src={chesster1.toString()} />
+                </div>
+                <span className="text-lg font-semibold">Die Week of Charity </span>lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sem sem, accumsan euismod venenatis sit amet, ornare a sem. Quisque consectetur quam sed consectetur venenatis. Mauris tristique ipsum eu gravida varius. Etiam elementum enim ac dolor vulputate, sed sodales leo tristique. Etiam gravida, quam at efficitur dictum, ligula leo luctus tortor, eu auctor sem est ut purus. Vestibulum nec ante purus. Donec lorem orci, eleifend quis facilisis vel, suscipit at tellus. Nullam magna ante, hendrerit sit amet ante vel, tristique faucibus arcu. Nulla facilisi. Nunc quis eros semper odio euismod feugiat eget quis lacus. <br /><br />
+                
+                <span className="text-lg font-semibold">In diesem Jahr </span>sed id aliquet libero. Ut venenatis vehicula laoreet. Phasellus ut auctor purus. Mauris porttitor nunc odio, et consequat arcu dapibus ut. Nam tristique viverra mi feugiat vehicula. Donec condimentum orci eget quam ullamcorper dapibus. Ut vehicula diam nec fringilla finibus. Maecenas imperdiet, turpis et pellentesque vehicula, nibh lacus egestas dui, id volutpat leo felis in enim. Vivamus dignissim tortor sit amet condimentum maximus. Sed consectetur vehicula orci in tempor. Nulla eu velit ultrices, venenatis purus id, tristique orci. Nam nunc nisl, luctus sed blandit non, laoreet quis mauris.
+                
+              </div>
             </div>
           </section>
 
+          <div className="font-pally font-bold max-w-screen-md mx-auto mt-20 mb-10 text-accent-500 text-2xl md:text-5xl w-4/5 text-center">
+            Bisherige Projekte
+          </div>
+
+          <div className="timeline">
+            <div className="line"></div>
+            <div className="card-container">
+              <div className="card bg-persian-100 bg-opacity-50 mx-12 mb-12">
+                <div className="info leading-relaxed py-5 pl-8 text-left">
+                  <h3 className="title font-bold">Tierheim Berlin</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed id aliquet libero. Ut venenatis vehicula laoreet. Phasellus ut auctor purus. Mauris porttitor nunc odio, et consequat arcu dapibus ut. Nam tristique viverra mi feugiat vehicula. Donec condimentum orci eget quam ullamcorper dapibus. Ut vehicula diam nec fringilla finibus. Maecenas imperdiet, turpis et pellentesque vehicula, nibh lacus egestas dui, id volutpat leo felis in enim. Vivamus dignissim tortor sit amet condimentum maximus. Sed consectetur vehicula orci in tempor. Nulla eu velit ultrices, venenatis purus id, tristique orci. Nam nunc nisl, luctus sed blandit non, laoreet quis mauris. </p>
+                </div>
+                <div className="timeline-image-wrapper-right">
+                  <img className="chesster-4" src={chesster4.toString()} />
+                </div>
+              </div>
+
+              <div className="card bg-persian-100 bg-opacity-50 mx-12 mb-12">
+                <div className="timeline-image-wrapper-left">
+                  <img className="chesster-3" src={chesster3.toString()} />
+                </div>
+                
+                <div className="info leading-relaxed py-5 pr-8 text-right">
+                  <h3 className="title font-bold">Queere Vielfalt leben</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed id aliquet libero. Ut venenatis vehicula laoreet. Phasellus ut auctor purus. Mauris porttitor nunc odio, et consequat arcu dapibus ut. Nam tristique viverra mi feugiat vehicula. Donec condimentum orci eget quam ullamcorper dapibus. Ut vehicula diam nec fringilla finibus. Maecenas imperdiet, turpis et pellentesque vehicula, nibh lacus egestas dui, id volutpat leo felis in enim. Vivamus dignissim tortor sit amet condimentum maximus. Sed consectetur vehicula orci in tempor. Nulla eu velit ultrices, venenatis purus id, tristique orci. Nam nunc nisl, luctus sed blandit non, laoreet quis mauris. </p>
+                </div>
+              </div>
+
+              <div className="card bg-persian-100 bg-opacity-50 mx-12 mb-12">
+                <div className="info leading-relaxed py-5 pl-8 text-left">
+                  <h3 className="title font-bold">Lesben- und Schwulenverband Deutschland</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed id aliquet libero. Ut venenatis vehicula laoreet. Phasellus ut auctor purus. Mauris porttitor nunc odio, et consequat arcu dapibus ut. Nam tristique viverra mi feugiat vehicula. Donec condimentum orci eget quam ullamcorper dapibus. Ut vehicula diam nec fringilla finibus. Maecenas imperdiet, turpis et pellentesque vehicula, nibh lacus egestas dui, id volutpat leo felis in enim. Vivamus dignissim tortor sit amet condimentum maximus. Sed consectetur vehicula orci in tempor. Nulla eu velit ultrices, venenatis purus id, tristique orci. Nam nunc nisl, luctus sed blandit non, laoreet quis mauris. </p>
+                </div>
+                <div className="timeline-image-wrapper-right">
+                  <img className="chesster-2" src={chesster2.toString()} />
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
       </div>
 
     </main>

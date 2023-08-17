d6ee3b97d1fd0a389df9cf027f4d3dca23841000
Bina <BinaKleinerAls3@gmail.com>
2023-08-17 17:41:25 +0200

    fix: cleaned up DonationGoal-Widget

--- src/pages/GoalWidget.tsx
+++ src/pages/GoalWidget.tsx
@@ -6,13 +6,13 @@ import { useDonations } from '../hooks/useDonations';
 import { getDocumentTitle } from '../utils/getDocumentTitle';
 import './GoalWidget.scss';
 
-
 export const GoalWidget = () => {
 
   document.title = getDocumentTitle('GoalWidget');
 
   const [currentDonation, setCurrentDonation] = useState<number | undefined>(0);
   const [nextDonationGoal, setNextDonationGoal] = useState<number | undefined>(undefined);
+  const [nextDonationGoalText, setNextDonationGoalText] = useState<string | undefined>(undefined);
 
   const { data: donations, status: donationsStatus } = useDonations();
   const { data: donationGoals, status: donationGoalsStatus } = useDonationGoals();
@@ -31,17 +31,43 @@ export const GoalWidget = () => {
     });
 
     setNextDonationGoal(donationGoals.length >= lastIndex + 2 ? donationGoals[lastIndex + 1].reached_at : undefined);
+    setNextDonationGoalText(donationGoals.length >= lastIndex + 2 ? donationGoals[lastIndex + 1].name : undefined);
   }, [donations, donationGoals]);
 
   return (
-      <div className="widgetWrapper">
-      {nextDonationGoal && (
-          <>
-            <div>{nextDonationGoal}</div>
-          </>
-        )}
-      <div className="donationMeter">uwu</div>
-    </div>
-    
+        <div className="widgetWrapper">
+          {nextDonationGoal && (
+            <>
+              <div className="donationNameWrapper">
+                <div className="donationName">
+                  {nextDonationGoalText}
+                </ div>  
+              </ div>  
+              <div className="donationBox">
+                <div className="donationBoxText">
+                  {currentDonation}€ von {nextDonationGoal}€
+                </ div>
+                <div
+                  className="donationFill"
+                  style={{
+                    backgroundColor: '#34b7eb',
+                    backgroundSize: '200% 100%',
+                    width: `${(currentDonation / nextDonationGoal)*100}%`,
+                    height: '100%',
+                    position: 'absolute',
+                }}></ div>
+              </ div>
+            </>
+          )}
+          {donations && donationGoals && !nextDonationGoal && (currentDonation>=donationGoals[donationGoals.length-1].reached_at) && (
+            <>
+              <div className="donationNameWrapper">
+                <div className='successMessage'>
+                  Alle Goals wurden erreicht!
+                </div>
+              </ div>
+            </>
+          )}
+        </ div>
   );
 };

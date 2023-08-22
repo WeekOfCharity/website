e5885a6275c1ca340d6e6f48f624ac08e3363c9f
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2023-08-22 23:01:35 +0200

    small fixes to images

--- src/pages/Program.tsx
+++ src/pages/Program.tsx
@@ -87,7 +87,7 @@ export const Program = () => {
                   <Stream
                     activityId={stream.activity.id}
                     endTime={stream.end}
-                    gameImageUrl={(process.env.NODE_ENV === 'production' ? 'https://directus.weekofcharity.de' : 'http://localhost:8055') + `/assets/${stream.activity.icon}`}
+                    gameImageUrl={(process.env.NODE_ENV === 'production' ? 'https://directus.weekofcharity.de' : 'http://localhost:8055') + `/assets/${stream.activity.icon}?width=1024&height=512&quality=75&fit=cover&format=webp`}
                     highlight={stream.highlight}
                     startTime={stream.start}
                     state={getState(stream.start, stream.end)}

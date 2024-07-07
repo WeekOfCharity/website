1f7c31b215949f09ad6f26ead6751e18e9c8c4c9
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2024-07-07 20:41:28 +0200

    fix: simplify useTitle hook

--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -3,12 +3,5 @@ import { getDocumentTitle } from "../utils/getDocumentTitle";
 
 export const useTitle = (title?: string) =>
   useEffect(() => {
-    const newTitle = getDocumentTitle(title);
-    document.title = newTitle;
-
-    return () => {
-      setTimeout(() => {
-        if (document.title === newTitle) document.title = getDocumentTitle();
-      }, 500);
-    };
+    document.title = getDocumentTitle(title);
   }, [title]);

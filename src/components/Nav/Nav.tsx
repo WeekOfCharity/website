6c163183ba6cb765fbe3d950b249773de4a08625
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2024-07-22 21:53:43 +0200

    feat: add bluesky social link with placeholder url

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,6 +4,7 @@ import {
   mdiMenu,
   mdiTwitter,
   mdiYoutube,
+  mdiMastodon,
 } from "@mdi/js";
 import Icon from "@mdi/react";
 import classNames from "classnames";
@@ -18,8 +19,8 @@ import { LanguageSwitch } from "../LanguageSwitch/LanguageSwitch";
 const logo = new URL("../../assets/favicon.png", import.meta.url);
 const tiktokPath =
   "M12.53.02C13.84 0 15.14.01 16.44 0c.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z";
-const mastodonPath =
-  "M21.327 8.566c0-4.339-2.843-5.61-2.843-5.61-1.433-.658-3.894-.935-6.451-.956h-.063c-2.557.021-5.016.298-6.45.956 0 0-2.843 1.272-2.843 5.61 0 .993-.019 2.181.012 3.441.103 4.243.778 8.425 4.701 9.463 1.809.479 3.362.579 4.612.51 2.268-.126 3.541-.809 3.541-.809l-.075-1.646s-1.621.511-3.441.449c-1.804-.062-3.707-.194-3.999-2.409a4.523 4.523 0 0 1-.04-.621s1.77.433 4.014.536c1.372.063 2.658-.08 3.965-.236 2.506-.299 4.688-1.843 4.962-3.254.434-2.223.398-5.424.398-5.424zm-3.353 5.59h-2.081V9.057c0-1.075-.452-1.62-1.357-1.62-1 0-1.501.647-1.501 1.927v2.791h-2.069V9.364c0-1.28-.501-1.927-1.502-1.927-.905 0-1.357.546-1.357 1.62v5.099H6.026V8.903c0-1.074.273-1.927.823-2.558.566-.631 1.307-.955 2.228-.955 1.065 0 1.872.409 2.405 1.228l.518.869.519-.869c.533-.819 1.34-1.228 2.405-1.228.92 0 1.662.324 2.228.955.549.631.822 1.484.822 2.558v5.253z";
+const blueSkyPath =
+  "M 5.203125 1.746094 C 7.75 3.722656 10.492188 7.734375 11.5 9.886719 C 12.507812 7.734375 15.25 3.722656 17.796875 1.746094 C 19.636719 0.316406 22.617188 -0.785156 22.617188 2.726562 C 22.617188 3.429688 22.226562 8.621094 22 9.464844 C 21.207031 12.398438 18.3125 13.144531 15.742188 12.691406 C 20.238281 13.484375 21.382812 16.101562 18.910156 18.722656 C 14.21875 23.699219 12.167969 17.476562 11.640625 15.878906 C 11.546875 15.585938 11.5 15.449219 11.5 15.566406 C 11.5 15.449219 11.453125 15.585938 11.359375 15.878906 C 10.832031 17.476562 8.78125 23.699219 4.089844 18.722656 C 1.617188 16.101562 2.761719 13.484375 7.257812 12.691406 C 4.6875 13.144531 1.792969 12.398438 1 9.464844 C 0.773438 8.621094 0.382812 3.429688 0.382812 2.726562 C 0.382812 -0.785156 3.363281 0.316406 5.203125 1.746094 Z M 5.203125 1.746094";
 
 export const Nav = () => {
   const { t } = useTranslation();
@@ -131,7 +132,7 @@ export const Nav = () => {
             </span>
           </NavLink>
         </div>
-        <div className="md:flex hidden items-center ml-auto space-x-5">
+        <div className="md:flex hidden items-center ml-auto space-x-5 pt-0.5">
           <a
             className="hover:text-neutral-500"
             href="https://twitter.com/WeekOfCharity/"
@@ -162,7 +163,7 @@ export const Nav = () => {
             target="_blank"
           >
             <span>
-              <Icon path={mastodonPath} size="1.25rem" />
+              <Icon path={mdiMastodon} size="1.25rem" />
             </span>
           </a>
           <a
@@ -184,7 +185,18 @@ export const Nav = () => {
             target="_blank"
           >
             <span>
-              <Icon path={mdiYoutube} size="1.25rem" />
+              <Icon path={mdiYoutube} size="1.5rem" />
+            </span>
+          </a>
+          <a
+            className="hover:text-neutral-500 pt-px"
+            href="https://bsky.app/profile/chesster.weekofcharity.de"
+            title="Bluesky"
+            rel="nofollow noreferrer"
+            target="_blank"
+          >
+            <span>
+              <Icon path={blueSkyPath} size="1.2rem" />
             </span>
           </a>
         </div>
@@ -339,7 +351,7 @@ export const Nav = () => {
               rel="nofollow noreferrer"
               target="_blank"
             >
-              <Icon path={mastodonPath} size="1.5rem" />
+              <Icon path={mdiMastodon} size="1.5rem" />
             </a>
             <a
               className="hover:text-neutral-500"
@@ -357,7 +369,16 @@ export const Nav = () => {
               rel="nofollow noreferrer"
               target="_blank"
             >
-              <Icon path={mdiYoutube} size="1.5rem" />
+              <Icon path={mdiYoutube} size="1.75rem" />
+            </a>
+            <a
+              className="hover:text-neutral-500 pt-0.5"
+              href="https://bsky.app/profile/chesster.weekofcharity.de"
+              title="Bluesky"
+              rel="nofollow noreferrer"
+              target="_blank"
+            >
+              <Icon path={blueSkyPath} size="1.5rem" />
             </a>
           </div>
         </menu>

0fcfdb52199d522370c8e61814ce62dca27b62c4
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2024-09-02 17:35:20 +0200

    feat: full english translation

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -4,14 +4,14 @@ export const enTranslation: Translation = {
   subNav: {
     donate: "Donate",
     music: "Music",
-    merch: "Merch",
-    dataPrivacy: "Data Privacy",
+    merch: "Merchandise",
+    dataPrivacy: "Privacy Policy",
     imprint: "Imprint",
   },
   mainNav: {
     home: "Home",
     projects: "Projects",
-    program: "Program",
+    program: "Schedule",
     activities: "Activities",
     team: "Team",
     gallery: "Gallery",
@@ -22,42 +22,168 @@ export const enTranslation: Translation = {
   },
   activities: {
     activities: "Activities",
-    subHeader: "All at a glance",
-    mainHeader: "The Activities of", // Week of Charity
-    joinTheseStreams: "Join these streams",
+    subHeader: "At a glance",
+    mainHeader: "Activities for the", // Week of Charity
+    joinTheseStreams: "Join us in these streams",
     isHostedBy: "is hosted by",
     isAccompaniedBy: "is accompanied by",
   },
   gallery: {
-    gallery: "Picture Gallery",
-    subHeader: "From you and us",
-    mainHeader: "The Gallery of", // Week of Charity
+    subHeader: "Created by you and us",
+    mainHeader: "Gallery of the", // Week of Charity
     year: "Year",
     category: "Category",
+    from: "Created by",
+    credit: "Credit:",
   },
   home: {
     upcomingText: "We are streaming again for a good cause",
     runningText: "We are streaming again for a good cause",
-    endedText: "Thanks for joining",
+    endedText: "Thank you for joining us!",
     subHeader: "New year, new week",
+    countdown: {
+      days: "Days",
+      hours: "Hours",
+      minutes: "Minutes",
+      seconds: "Seconds",
+    },
     weekOfWhat: "Week of What?",
     aboutProject: "About the project",
     welcome1:
-      "Welcome! We are the Week of Charity, a continuous streaming project for a good cause! For one week, streaming will be continuous and alternate on the Twitch channels of our members to collect donations. The program is diverse and in addition to various video games, Dungeons and Dragons will be played and quiz shows will be held.",
+      "Welcome! We are the Week of Charity, a project designed to stream once a year for a good cause. For one week, different members of our group will alternate in streaming their activities to collect donations. We have a diverse program in addition to various video games, real life projects like Dungeons and Dragons and quiz shows will be broadcasted.",
     welcome2:
-      "All donations collected during this week will go to the Berlin Animal Shelter this year. More about our project can be found",
+      'This year all donations will go to the project "EXIT Deutschland", a charity dedicated to helping people exit right-wing extremism. More information can be found',
     here: "here",
     welcome3:
-      "We hope the week brings you as much joy and entertainment as it does us. We look forward to seeing you!",
+      "We hope this week will be as fun and entertaining to you as it will be to us! We look forward to seeing you!",
     entertainment1: "Entertainment",
     entertainment2: "without end",
     fullProgram: "The full program",
-    weAreBehindIt1: "We are",
-    weAreBehindIt2: "behind it",
-    participants: "Participants and helpers",
+    weAreBehindIt1: "Behind",
+    weAreBehindIt2: "the scenes",
+    participants: "Team and Support",
     donateNow: "Donate now",
     faq: "Frequently Asked Questions",
   },
-  clickForMoreInfo: "Click us for more info",
-  seeMoreSoon: "You'll see more here soon!",
+  twitchPlayer: {
+    heading: "Week of Charity Twitch Player",
+    privacyNote:
+      "Data Protection Notice: By activating the Twitch Player, data like your IP address will be sent to Twitch directly. This serves as improvement to your streaming experience and the data will be used for analyses..",
+    accept: "Accept",
+  },
+  imprint: {
+    paragraph: "Information according § 5 TMG",
+    email: "E-Mail Address",
+    disclaimer: "Disclaimer",
+    content: {
+      headline: "Liability for contents",
+      text: "The contents of our websites have been created with utmost care. However, we cannot accept any liability for the accuracy, completeness and actuality of the content. As a service provider, we are responsible for our own content on these pages in accordance with § 7 para. 1 TMG (German Telemedia Act) and general laws. According to §§ 8 to 10 TMG, however, we as a service provider are not obliged to monitor transmitted or stored third-party information or to investigate circumstances that indicate illegal activity. Obligations to remove or block the use of information in accordance with general legislation remain unaffected by this. However, liability in this respect is only possible from the time of knowledge of a specific infringement. As soon as we become aware of such infringements, we will remove the content immediately.",
+    },
+    links: {
+      headline: "Liability for external links",
+      text: "Our website contains links to third-party websites, whose content we can't influence. Thus we are not responsible for the displayed content. Reliable for the content on third-party websites are always the respective owners. The linked websites were checked on illegal content before beeing integrated on our website and thus no illegal contents were noticeable. A constant check of the contents of the third-party websites without any reasoning or Violation of law is not reasonable. If violations of law become known, the associated links will be deleted.",
+    },
+  },
+  music: {
+    mainHeader: "Our Music",
+    text: {
+      t1: "Like every year",
+      t2: "our music is made by", // Amy and Mioh
+      t3: "You can buy the songs on Bandcamp. All profits will be donated to the current charity. To the bottom you can find a list of every Week of Charity album that were produced over time. If you click on an album, you will be forwarded to the respective Bandcamp website were you can buy it. Thank you for your support!",
+    },
+  },
+  program: {
+    subHeader: "Don't miss any events",
+    mainHeader: "Program of the", // Week of Charity
+    highlights: "Highlights of the week",
+    allStreams: "Every Stream",
+    fellows: "fellow players", // in the context of: "+4 fellow players"
+    toVOD: "open VOD",
+    VODavailable: "VOD available",
+    highlight: "👑 Highlight",
+  },
+  projects: {
+    subHeader: "About Us",
+    mainHeader: "The Week of Charity",
+    text: {
+      t1: "The Week of Charity",
+      t2: "is a constant streaming project for a good cause! During one whole week you are able to see constant streams on Twitch from on of our",
+      t3: "members",
+      t4: "to collect donations. The program is made up of a variety of entertaining streams containing different video games, Dungeons and Dragons and even multiple quiz shows! You don't want to miss anything Special? Look at our",
+      t5: "Schedule",
+      t6: ".",
+      t7: "Our project",
+      t8: "was established in 2021 and has now it's fourth anniversairy! The money that is gathered during this week will be donated to Exit Germany. You can support us either with direct",
+      t9: "donations",
+      t10: "during the streams or by buying merchandise from our",
+      t11: "shop",
+      t12: "The whole profit gathered by the merchandising sales will be donated to the current charity and you will be able to wear our mascot Chesster!",
+    },
+    ourProjects: "Our Projects",
+    "2023": {
+      header: "Tierheim Berlin",
+      text: {
+        t1: "For our third event in 2023 we gathered donations for the",
+        t2: "Tierheim Berlin",
+        t3: ", the biggest pet shelter in germany's capital city. The shelter gives a home to cats, dogs, farm animals and even some exotic animals, thus taking care of over 1300 animals. During the Week of Charity 2023 we gathered over 2600€ for the Tierheim Berlin to support their work helping and caring for their animals.",
+      },
+    },
+    "2022": {
+      header: "Queere Vielfalt leben",
+      text: {
+        t1: "For our second gathering in 2022 we chose the incentive of the LGBTQ+ youth dormitory by",
+        t2: "Queere Vielfalt leben",
+        t3: ". The project supports the queer youth and gives them a sensitive and loving accomodation in Cologne. The young queer people will be guided by social workers to discover their identity and to be able to live by themselves. Workers and young people from the project even joined our streaming team and contributed to our streams by playing online games with us! At the end we were able to gather over 3500€, which were used to renovate the basement of the dormitory.",
+      },
+    },
+    "2021": {
+      header: "Lesben- und Schwulenverband in Deutschland",
+      text: {
+        t1: "The",
+        t2: "Lesben- und Schwulenverband in Deutschland",
+        t3: "(for short: LSVD) was the first organization we supported through our Week of Charity 2021. The LSVD is a civil rights association which campaigns for the rights and interests of lesbian, gay, bisexual, transgender, intersex and queer people. Our donations went specifically to the Covid Aid Africa project, in which the LSVD and local partners supported queer people in Africa who, as a minority, are often particularly affected by crisis situations. They are scapegoated for virus outbreaks, which increases hatred towards them and causes them to lose their jobs or family support faster than others. There was also a lack of critical goods such as masks, disinfectants and hygiene products. To support these people, we were able to raise an incredible €2,500 in total at our first ever WoC thanks to your help!",
+      },
+    },
+  },
+  team: {
+    subHeader: "Helpers and friends",
+    mainHeader: "The team of the", // Week of Charity
+    weStream: "Streaming",
+    weSupport: "Supporting",
+    playMusic: "play music",
+    hosts: "hosts",
+    supports: "supports",
+    roles: {
+      Design: "Design",
+      Musik: "Music",
+      Website: "Website",
+      SocialMedia: "Social Media",
+    },
+  },
+  goals: {
+    beThere: "Be there when this goal is redeemed",
+    tbd: "Date still pending",
+    secret: "Shh..this donation goal is still a secret",
+  },
+  bidwar: {
+    vote: "Vote by donating",
+    result: "end result",
+  },
+  donationMeter: {
+    current: "Current",
+    amount: "amount",
+    donationTarget: "current donation goal",
+    allTargetsReached: "All goals achieved!",
+  },
+  player: {
+    currentStream: "Current Stream",
+    notActive1: "Currently, the Week of Charity is not ongoing.",
+    notActive2: "To not miss any events, have a look at our schedule.",
+    loading: "Current stream loading...",
+    watchNow: "watch now",
+  },
+  and: "und",
+  clickForMoreInfo: "Click here for more information",
+  seeMoreSoon: "You'll see more soon!",
+  viewDetails: "view details",
 };
